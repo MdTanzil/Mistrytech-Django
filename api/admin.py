727dc3840563984c5efc84fe6b51c745a3a199c5
Md Tanzil <mtanzil1999@gmail.com>
2024-05-08 21:07:40 +0600

    add phone number in user

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 # Register your models here.
-from .models import Category, SubCategory, Discount, Product, Image
-
+from .models import Category, SubCategory, Discount, Product, Image,Variant, User
+from django.contrib.auth.admin import UserAdmin
 
 class ImageInline(admin.TabularInline):
     model = Image
@@ -16,6 +16,7 @@ class ProductAdmin(admin.ModelAdmin):
         "category",
         "subcategory",
         "price",
+        'discounted_price',
         "quantity",
         "image_display",
         "is_active",
@@ -27,6 +28,9 @@ class ProductAdmin(admin.ModelAdmin):
     ]  # Include ImageInline to display images related to the product
     list_per_page = 20  # Number of items per page
     ordering = ('-id',)  # Sort by ID in descending order
+    
+    def discounted_price(self, obj):
+        return obj.discounted_price
 
     def image_display(self, obj):
         images_html = ""
@@ -35,7 +39,7 @@ class ProductAdmin(admin.ModelAdmin):
         ):  # Assuming 'images' is the related_name for the Image model
             for image in obj.images.all():
                 images_html += (
-                    f'<img src="{image.image.url}" width="50" height="50" />&nbsp;'
+                    f'<img src="{image.image.url}" width="40" height="40" />&nbsp;'
                 )
         else:
             images_html = "No Images"
@@ -83,20 +87,48 @@ class SubCategoryAdmin(admin.ModelAdmin):
 
 class DiscountAdmin(admin.ModelAdmin):
     list_display = (
-        "product",
+      
         "discount_value",
         "description",
         "start_date",
         "end_date",
         "is_active",
     )
-    list_filter = ("is_active",'end_date','start_date')
-    search_fields = ["product__name", "description",'end_date','start_date']
+    list_filter = ("is_active",'end_date','start_date',"product")
+    search_fields = ["description",'end_date','start_date']
     list_per_page = 20  # Number of items per page
     ordering = ('-id',)  # Sort by ID in descending order
 
+class VariantAdmin(admin.ModelAdmin):
+    list_display = (
+      
+        "product",
+        "discount",
+        "size",
+        "price",
+        'quantity',
+        "is_active",
+    )
+    list_filter = ("is_active","product")
+    # search_fields = ['end_date','start_date']
+    list_per_page = 20  # Number of items per page
+    ordering = ('-id',)  # Sort by ID in descending order
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'phone_number', 'is_staff')
+     # Add phone_number to fieldsets and add_fieldsets
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),  # Include phone_number here
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Discount, DiscountAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variant,VariantAdmin)
+admin.site.register(User, CustomUserAdmin)
--- api/models.py
+++ api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-
+from decimal import Decimal
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
@@ -34,13 +35,6 @@ class SubCategory(models.Model):
 
 
 class Discount(models.Model):
-    product = models.ForeignKey(
-        'Product',
-        on_delete=models.CASCADE,
-        related_name="discounts",
-        null=True,
-        blank=True,
-    )
     discount_value = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True
     )
@@ -51,6 +45,8 @@ class Discount(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.discount_value}% discount"
 
 
 class Product(models.Model):
@@ -74,7 +70,7 @@ class Product(models.Model):
     discount = models.ForeignKey(
         Discount,
         on_delete=models.CASCADE,
-        related_name="products",
+        related_name="product",
         null=True,
         blank=True,
     )
@@ -84,9 +80,18 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
 
+    @property
+    def discounted_price(self):
+        if self.discount:
+            discount_value = self.discount.discount_value
+            if discount_value is not None:
+                discount_amount = self.price * (discount_value / Decimal(100))
+                discounted_price = self.price - discount_amount
+                return discounted_price
+        return self.price
+
     def __str__(self):
         return self.name
-
 
 
 class Image(models.Model):
@@ -106,7 +111,50 @@ class Image(models.Model):
     image = models.ImageField(upload_to="images/")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
+
+class Variant(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="variants",
+        null=True,
+        blank=True,
+    )
+    discount = models.ForeignKey(
+        Discount,
+        on_delete=models.CASCADE,
+        related_name="variants",
+        null=True,
+        blank=True,
+    )
+    size = models.FloatField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    @property
+    def discounted_price(self):
+        if self.discount:
+            discount_value = self.discount.discount_value
+            if discount_value is not None:
+                discount_amount = self.price * (discount_value / Decimal(100))
+                discounted_price = self.price - discount_amount
+                return discounted_price
+        return self.price
+
+    def __str__(self):
+     return f"Variant of {self.product.name if self.product else 'price'}"
+
+
+class User (AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 
 # Signals to generate slug for Category, SubCategory, and Product
 @receiver(pre_save, sender=Category)
